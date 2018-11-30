f671fc62b1383c3d5aabaeff609c207bda26a885
Dmitry Streltsov <vostlerts@yandex.com>
2018-11-30 20:30:45 +0300

    Added HTTPs

--- src/app/utils/constants.js
+++ src/app/utils/constants.js
@@ -1,2 +1,2 @@
-export const apiBaseURL = 'http://api.openweathermap.org/data/2.5';
+export const apiBaseURL = 'https://api.openweathermap.org/data/2.5';
 export const apiKey = '84abd92f7630e594bd40bdaa8b96fc07';
